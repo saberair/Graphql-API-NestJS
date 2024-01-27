6bfe4d21ed6f0d0d9ff08a233aad7ba2045939b4
Saber CHAABANE <saber.chaabane-ext@sanofi.com>
2024-01-27 16:45:12 +0100

    feat: add nestjs module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { CoffeeModule } from './coffee/coffee.module';
 import { OrderModule } from './order/order.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CoffeeModule, OrderModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema/schema.gql',
+      introspection: true,
+      playground: true,
+    }),
+    CoffeeModule,
+    OrderModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeeService } from './coffee.service';
 import { CoffeeResolver } from './coffee.resolver';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [CoffeeResolver, CoffeeService],
 })
 export class CoffeeModule {}
