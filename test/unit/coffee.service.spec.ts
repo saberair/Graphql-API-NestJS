735e452c62d5b148f64eb9fafafd8cc82172fdc2
Saber CHAABANE <saber.chaabane-ext@sanofi.com>
2024-01-27 15:57:24 +0100

    test: prepare unit tests for services

--- test/unit/coffee.service.spec.ts
+++ test/unit/coffee.service.spec.ts
@@ -2,17 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeeService } from '../../src/coffee/coffee.service';
 
 describe('CoffeeService', () => {
-  let service: CoffeeService;
+  let coffeeService: CoffeeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CoffeeService],
     }).compile();
 
-    service = module.get<CoffeeService>(CoffeeService);
+    coffeeService = module.get<CoffeeService>(CoffeeService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(coffeeService).toBeDefined();
+  });
+
+  it('finds all coffees', async () => {
+    await coffeeService.findAll();
+  });
+
+  it('finds one coffee', async () => {
+    await coffeeService.findOne();
+  });
+
+  it('creates a new coffee', async () => {
+    await coffeeService.create();
+  });
+
+  it('updates a coffee', async () => {
+    await coffeeService.update();
+  });
+
+  it('removes a coffee', async () => {
+    await coffeeService.remove();
   });
 });
--- test/unit/order.service.spec.ts
+++ test/unit/order.service.spec.ts
@@ -2,17 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from '../../src/order/order.service';
 
 describe('OrderService', () => {
-  let service: OrderService;
+  let orderService: OrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [OrderService],
     }).compile();
 
-    service = module.get<OrderService>(OrderService);
+    orderService = module.get<OrderService>(OrderService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(orderService).toBeDefined();
+  });
+
+  it('finds all orders', async () => {
+    await orderService.findAll();
+  });
+
+  it('creates a new order', async () => {
+    await orderService.create();
   });
 });
